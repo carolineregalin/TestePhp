6cc5e157fc7322e395ef84ba079769c039193f8f
carolineregalin <33700047+carolineregalin@users.noreply.github.com>
2018-02-08 16:20:33 -0200

    Ajuste tela login
    Ajuste tela login

--- Cadastraki/application/views/login/index.php
+++ Cadastraki/application/views/login/index.php
@@ -1,35 +1,37 @@
 
 <html>
     <head>
+        <link rel ="stylesheet" href=" <?= base_url($uri = "css/login.css") ?> " >
         <link rel ="stylesheet" href=" <?= base_url($uri = "css/bootstrap.css") ?> " >
         <title>Cadastraki - Login</title>
     </head>
+    
     <body>
-    <div class="col-md-4">         
-    </div>     
-    <div class ="container">  
-        <div class="row">
-            <div class="col-md-4">
-                <div class="form-wrap">
-                    <h1>Login</h1>       
-                    <form action = "index.php/user_login/autenticar" method="post" enctype="multipart/form-data" autocomplete="off">
-                        <div class="form-group">
-                            <label>Nome:</label>
-                            <input  type="text" class = "form-control" name="nome" value="<?= set_value('nome') ?>" required/>
+   
+        <div class ="container">  
+            <div class="card card-container">
+                <div class="row">
+                   
+                        <div class="form-signin">
+                            <h2>Entrar</h2>       
+                            <form action = "index.php/user_login/autenticar" method="post" enctype="multipart/form-data" autocomplete="off">
+                                <div class="form-group">
+                                    <label>Nome:</label>
+                                    <input  type="text" class = "form-control" id ="nome" name="nome" required/>
+                                </div>
+                                <div class="form-group">
+                                    <label>Senha:</label>
+                                    <input type="password" class = "form-control" id ="senha" name="senha" required/>
+                                </div>
+                                <div>
+                                    <input type="submit"  value="Entrar" id="cadastrar" name="entrar" class="btn btn-lg btn-primary btn-block btn-signin"/>
+                                </div>
+                            </form>
                         </div>
-                        <div class="form-group">
-                            <label>Senha:</label>
-                            <input type="password" class = "form-control" name="senha" value="<?= set_value('senha') ?>" required/>
-                        </div>
-                        <div>
-                            <input type="submit" value="Entrar" id="cadastrar" name="entrar" class= "btn btn-primary"/>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="col-md-4">
-    </div>
-</body>    
+        
+            
+    </body>    
 </html>
