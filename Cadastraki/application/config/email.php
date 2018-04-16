6ae78b257814b2f186ff46f476eef47c7a84fb55
carolineregalin <33700047+carolineregalin@users.noreply.github.com>
2018-04-16 20:06:49 -0300

    Update email.php

--- Cadastraki/application/config/email.php
+++ Cadastraki/application/config/email.php
@@ -3,7 +3,7 @@
 $config['smtp_host'] = 'ssl://smtp.gmail.com';
 $config['smtp_port'] = 465;
 $config['smtp_user'] = 'cadastraki@gmail.com';
-$config['smtp_pass'] = 'c4d4str4';
+$config['smtp_pass'] = '';
 $config['protocol']  = 'smtp';
 $config['validate']  = TRUE;
 $config['mailtype']  = 'html';
